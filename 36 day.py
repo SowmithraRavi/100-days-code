055e8c21490fcec7302eefe5d4c799f2a593b126
SowmithraRavi <140725900+SowmithraRavi@users.noreply.github.com>
2023-08-14 20:06:51 +0530

    factorial and Fibonaccipy

--- 36 day.py
+++ 36 day.py
@@ -8,16 +8,21 @@ class Solution:
             for i in range(2, N + 1):
                 result *= i
             return result
-#fibonacci
+#fibonacci number 
 class Solution(object):
-    
     def fib(self, n):
-        
-        if n <= 0:
-            return 0
-        elif n == 1 or n == 2:
+        if(n==0):
+            return 0 
+        elif(n==1):
             return 1
         else:
-            return self.fib(n-1) + self.fib(n-2)
+            a=0 
+            b=1 
+            for i in range(2,n+1): 
+             c=a+b 
+             a=b 
+             b=c 
+
+            return c
 
 
