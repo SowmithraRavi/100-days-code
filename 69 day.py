f98823035d1729a2f0ee0764efe85ad140949523
SowmithraRavi <140725900+SowmithraRavi@users.noreply.github.com>
2023-11-19 10:07:07 +0530

    Rotate image.py

---
+++ 69 day.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def rotate(self, matrix):
+        for k in range(len(matrix)):
+            for j in range(len(matrix)-1, -1, -1):
+                matrix[k].append(matrix[j].pop(0))
+        
