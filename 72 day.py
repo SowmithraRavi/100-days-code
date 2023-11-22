a1644d6b7f91aa6418a6f1f7d7b5dfa743a0549d
SowmithraRavi <140725900+SowmithraRavi@users.noreply.github.com>
2023-11-22 07:43:54 +0530

    Pascal's Triangle.py

---
+++ 72 day.py
@@ -0,0 +1,16 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        if numRows == 0:
+            return []
+        if numRows == 1:
+            return [[1]]
+        
+        prev = self.generate(numRows - 1)
+        new= [1] * numRows
+        
+        for i in range(1, numRows - 1):
+            new[i] = prev[-1][i - 1] + prev[-1][i]
+        
+        prev.append(new)
+        return prev
+        
